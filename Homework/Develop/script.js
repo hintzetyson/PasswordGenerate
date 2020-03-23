3770b0bbb6d18450f7147bfddaa045a001f21494
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-03-23 12:57:32 -0600

    I've got everything but for some reason keep getting a weird prompt about the object after generating a password

--- Homework/Develop/script.js
+++ Homework/Develop/script.js
@@ -6,28 +6,38 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
 
-
-  var lengthprompt=prompt("What would you like the length of your password to be?");
+//Get the length of the password, returns to the start if it isn't correct
+  var lengthprompt=prompt("What would you like the length of your password to be? It must be between 8 and 128 characters");
   lengthprompt = parseInt(lengthprompt);
   if (lengthprompt < 8) {
     alert("Your password must be at least 8 characters.");
-    passwordParameters();
+    writePassword();
   }
   if (lengthprompt > 128 ) {
     alert("Your password must be less than 128 characters.")
-    passwordParameters();
+    writePassword();
   }
 
+// Confirming the rest of the parameters
   var uppercaseconfirm=confirm("Would you like uppercase letters?");
   var lowercaseconfirm=confirm("Would you like lowercase letters?");
   var specialcharactersconfirm=confirm("Would you like special characters?");
   var numbersconfirm=confirm("Would you like numbers?");
+
+  if (uppercaseconfirm ==false && lowercaseconfirm == false && specialcharactersconfirm == false && numbersconfirm == false) {
+    alert("You must select at least 1 parameter, program will now start over");
+    writePassword();
+
+  }
+
+  //Setting some variables
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var Numbers = "1234567890";
   var Specialcharacters = "!@#$%^&*()_+-':;<>={}[]\/.?~ ";
   var includedChars = "";
 
+  //Checking the confirm of the variables
   if(lowercaseconfirm){
     includedChars += lowercase;
   }
@@ -41,13 +51,17 @@ function writePassword() {
       includedChars += Specialcharacters;  
     }
 
+
+  //Looping through the password length to get all characters
   for(var i=0; i < lengthprompt; i++){
-      var randomNumber = getRandom(0,includedChars.length);
+      var randomNumber = Math.floor(Math.random()*includedChars.length-1);
       password += includedChars.charAt(randomNumber);
-      console.log(lowercase.charAt(randomNumber));
+      console.log(includedChars.charAt(randomNumber));
     }
   
-  var passwordText = document.querySelector("#password");
+
+  //Putting the text on the screen
+  var passwordText = document.getElementById("password");
   
   passwordText.value = password;
 
