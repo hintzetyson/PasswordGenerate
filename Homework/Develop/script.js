f1796258ad3121738d75f0e778829ac5348d4bc7
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-03-23 11:56:30 -0600

    Starting code

--- Homework/Develop/script.js
+++ Homework/Develop/script.js
@@ -5,41 +5,41 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  var uppercase=confirm("Would you like uppercase letters?")
-  var lowercase=confirm("Would you like lowercase letters?")
-  var specialcharacters=confirm("Would you like special characters?")
-  var numbers=confirm("Would you like numbers?")
+  var uppercaseconfirm=confirm("Would you like uppercase letters?");
+  var lowercaseconfirm=confrim("Would you like lowercase letters?");
+  var specialcharactersconfirm=confirm("Would you like special characters?");
+  var numbersconfirm=confirm("Would you like numbers?");
 
-  var password = generatePassword() {
-  var allChars = "abcdefghijklmnopqrstuvwxyz";
-  var charsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var charNumbers = "1234567890";
-  var charSpecial = "!@#$%^&*()_+-':;<>={}[]\/.?~ ";
-  var ret = "";
-  if(upperCase){
-    allChars += charsUpper;
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var Numbers = "1234567890";
+  var Specialcharacters = "!@#$%^&*()_+-':;<>={}[]\/.?~ ";
+  var includedChars = "";
+
+  if(lowercaseconfirm){
+    includedChars += lowercase;
   }
-  if(numbers){
-    allChars += charNumbers;
-  }
-  if(special){
-    allChars += charSpecial;  
-  }
+  if(uppercaseconfirm){
+      includedChars += Uppercase;
+    }
+  if(numbersconfirm){
+      includedChars += Numbers;
+    }
+  if(specialcharactersconfirm){
+      includedChars += Specialcharacters;  
+    }
   //finding chars from included chars
-  for(var i=0; i < chars; i++){
-    var randomNumber = getRandom(0,allChars.length);
-    ret += allChars.charAt(randomNumber);
-    console.log(allChars.charAt(randomNumber));
-  }
-  return ret;
-}
-  };
-
+  for(var i=0; i < chosenNumber; i++){
+      var randomNumber = getRandom(0,includedChars.length);
+      password += includedChars.charAt(randomNumber);
+      console.log(lowercase.charAt(randomNumber));
+    }
+  
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
-}
+  
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
